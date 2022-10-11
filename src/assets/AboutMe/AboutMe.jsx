b1f494e032578ad9019087478120cf6586d6eb28
1zach <zachjamesthomas@gmail.com>
2022-10-11 09:39:58 +0900

    bundled

--- src/assets/AboutMe/AboutMe.jsx
+++ src/assets/AboutMe/AboutMe.jsx
@@ -2,9 +2,9 @@ export default function AboutMe() {
     return (
         <div className = "w-96 text-left px-10 my-6">
             <h2 className = "text-xl font-bold pb-2 text-neutral-100">About</h2>
-            <p className = "text-neutral-100">I'm a recent LeWagon graduate looking to transition from a 15 year career in teaching to a FullStack Dev job</p>
+            <p className = "text-neutral-200">I'm a recent LeWagon graduate looking to transition from a 15 year career in teaching to a FullStack Dev job</p>
             <h2 className = "text-xl font-bold pb-2 text-neutral-100">Interests</h2>
-            <p className = "text-neutral-100">Ruby on Rails, HTML, CSS, vanilla JS, React</p>        
+            <p className = "text-neutral-200">Ruby on Rails, HTML, CSS, vanilla JS, React</p>        
         </div>
     )
 }
